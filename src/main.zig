f4754fb9ba60a46afc98531bb16a5d29d0c37bc5
David <daniel.david.surla@gmail.com>
2023-03-26 22:43:45 +0100

    updated all the necessary files

--- src/main.zig
+++ src/main.zig
@@ -4,28 +4,17 @@ const fs = std.fs;
 const mem = std.mem;
 const zbs = std.build;
 const raylib = @cImport({
-    @Include("raylib.h");
+    @cInclude("raylib.h");
 });
 
 const File = &[_][]const u8{};
 const String = *const []u8;
 
-fn initialise_window_application() !void {
-    const window_screen_width = 1280;
-    const window_screen_height = 1024;
-    const window_application_title = "Night 12";
-
-    raylib.InitWindow(window_screen_width, window_screen_height, window_application_title);
-    defer raylib.CloseWindow();
-
-    raylib.SetTargetFPS(10);
-
-    while (!raylib.WindowShouldClose()) {
-        raylib.BeginDrawing();
-        defer raylib.EndDrawing();
-
-        raylib.ClearBackground(raylib.BLANK);
-    }
+fn initialise_odin_tx() !void {
+    const exe = builder.addExecutable("main", null);
+    // exe.addCSourceFile("main.c", File);
+    // exe.linkLibC();
+    exe.install();
 }
 
 fn add(a: i32, b: i32) callconv(.C) i32 {
@@ -33,13 +22,13 @@ fn add(a: i32, b: i32) callconv(.C) i32 {
 }
 
 fn build(builder: *std.build.Builder) void {
-    const exe = b.addExecutable("main", null);
+    const exe = builder.addExecutable("main", null);
     exe.addCSourceFile("main.c", File);
     exe.linkLibC();
     exe.install();
 }
 
 fn main() !void {
-    initialise_window_application();
+    initialise_odin_tx()();
     try add(1, 1);
 }
