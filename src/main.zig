22523c1b191a60cb7eac0d6bed343ed0ac1fde85
David <daniel.david.surla@gmail.com>
2023-03-27 06:27:42 +0100

    Update main.zig

--- src/main.zig
+++ src/main.zig
@@ -58,21 +58,21 @@ fn curle(input_uri_path: *const []u8) !void { // try keyword with `!void` return
     if (cURL.curl_easy_perform(handle) != cURL.CURLE_OK) {
         return error.FailedToPerformRequest;
     }
-} // compile with `zig build-exe zig-curl-test.zig --library curl --library c $(pkg-config --cflags libcurl)`
+} // compile with `zig build-exe ./src/main.zig --library curl --library c $(pkg-config --cflags libcurl)`
 
 fn setup() callconv(.C) void {
     inlaid.setup();
 }
 
-fn build(builder: *std.build.Builder) void {
+fn build(builder: *zbs.Builder) void {
     const exe = builder.addExecutable("main", null);
     exe.addCSourceFile("main.c", File);
     exe.linkLibC();
     exe.install();
 }
 
-fn main() !void {
-    const builder = *std.build.Builder;
+pub fn main() void {
+    const builder = *zbs.Builder;
     build(builder);
     try setup();
     try curle();
