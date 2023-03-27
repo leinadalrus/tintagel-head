1e7ba779848873d4fca34297aa4d98c80a744913
David <daniel.david.surla@gmail.com>
2023-03-27 21:50:37 +0100

    Update lib.rs

--- clib/src/lib.rs
+++ clib/src/lib.rs
@@ -1,14 +1,31 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![allow(unused_must_use)]
+use libfuzzer_sys::fuzz_target;
+
+#[derive(Debug, Clone, arbitrary::Arbitrary)]
+pub struct CommandlineArgument {
+    dest: Vec<u8>,
+    src: Vec<u8>,
+    n: usize,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn exploit_memcpy(dest: Vec<u8>, src: Vec<u8>, n: usize) {
+    let a = dest.leak();
+    let b = src.clone().leak();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    loop {
+        let mut i = 0;
+        while i < n {
+            a.len() - b.len();
+            i += 1;
+        }
+
+        if i > n { break; }
     }
+}
+
+fn main() -> std::result::Result<(), std::boxed::Box<dyn std::error::Error>> {
+    fuzz_target!(|args: CommandlineArgument| {
+        exploit_memcpy(args.dest, args.src, args.n);
+    });
+    Ok(())
 }
