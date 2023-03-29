fefdc4708182c764455781960a27468e6d1b6a26
David <daniel.david.surla@gmail.com>
2023-03-29 11:35:49 +0100

    Update mod.rs

--- clib/src/bin/mod.rs
+++ clib/src/bin/mod.rs
@@ -1,5 +1,10 @@
 #![allow(dead_code)]
-use sqlx::{self, postgres::PgPoolOptions, Pool, Postgres};
+use sqlx::{
+    self,
+    pool::PoolOptions,
+    postgres::{PgConnectOptions, PgPoolOptions},
+    Pool, Postgres, PgPool,
+};
 use thiserror::Error;
 
 #[derive(Error, Debug, Clone)]
@@ -55,13 +60,56 @@ impl DatabaseConfig {
             database_port,
         })
     }
+
+    pub fn init() -> DatabaseConfig {
+        dotenv::dotenv().ok();
+
+        let database_url =
+            std::env::var("DATABASE_URL").ok().map_or_else(|| "DATABASE_URL", |_| "DATABASE_URL" );
+
+        let database_port = std::env::var("PORT")
+            .ok()
+            .map_or_else(|| 443, |_| 80);
+
+        DatabaseConfig {
+            database_url: database_url.to_owned(),
+            database_port,
+        }
+    }
+}
+
+pub struct DatabaseHandler {
+    database_pool: PoolOptions<Postgres>,
+    database_config: DatabaseConfig,
+}
+
+impl DatabaseHandler {
+    pub async fn run(self) {
+        let database: &Pool<Postgres> = &PgPoolOptions::connect_lazy_with(
+            self.database_pool,
+            PgConnectOptions::default(),
+        );
+        connect_to_database(&self.database_config.database_url);
+        migrate_with_pooling(database);
+    }
+}
+
+impl Default for DatabaseHandler {
+    fn default() -> Self {
+        let database_pool = PgPoolOptions::default();
+        let database_config = DatabaseConfig::init();
+        return DatabaseHandler {
+            database_pool,
+            database_config,
+        };
+    }
 }
 
 fn undefined(args: &str) -> EErrors {
     EErrors::NotFound(format!("Error: {}", args))
 }
 
-pub async fn connect_to_datbase(
+pub async fn connect_to_database(
     database_url: &str,
 ) -> Result<Pool<Postgres>, EErrors> {
     PgPoolOptions::new()
@@ -88,6 +136,8 @@ async fn migrate_with_pooling(
 }
 
 fn main() -> std::result::Result<(), std::boxed::Box<dyn std::error::Error>> {
+    let database_handler = DatabaseHandler::default();
+    DatabaseHandler::run(database_handler);
     println!("cargo:rerun-if-changed=migrations");
     Ok(())
 }
