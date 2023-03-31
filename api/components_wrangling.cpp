19d0367e8028f6e30585687e75396b191db499f8
David <daniel.david.surla@gmail.com>
2023-03-31 23:31:37 +0100

    Update components_wrangling.cpp

--- api/components_wrangling.cpp
+++ api/components_wrangling.cpp
@@ -3,7 +3,8 @@
 #include "../lib/portaudio/include/portaudio.h"
 #include "../lib/libsndfile/include/sndfile.h"
 
-template <class T, class E> Result<T, E>::Result(T type, E expected) {}
+template <class T, class E>
+Result<T, E>::Result(T type, E expected) {}
 
 void UserHandler::handle_user_command(UserCommand user_command) {}
 
@@ -11,15 +12,40 @@ static int audio_test_callback(const void *input_buffer, void *output_buffer,
                                unsigned long frames_per_buffer,
                                const PaStreamCallbackTimeInfo *time_info,
                                PaStreamCallbackFlags status_flags,
-                               void *user_data) {
-  AudioTestData *audio_tester = (AudioTestData*)user_data;
-  float *output = (float*)output_buffer;
+                               void *user_data)
+{
+  AudioTestData *audio_tester = (AudioTestData *)user_data;
+  float *output = (float *)output_buffer;
   unsigned int index = 0;
 
   // for (index = 0; index < frames_per_buffer; index++) {
   //   output += audio_tester->left_channel_phase;
   //   output += audio_tester->right_channel_phase;
   // }
+
+  return 0;
+}
+
+constexpr bool init_pa_system()
+{
+  auto pa_err = Pa_Initialize();
+  if (pa_err == paNoError) {
+    pa_err = Pa_Terminate();
+  }
+  printf("PortAudio error: %s\n", Pa_GetErrorText(pa_err));
+
+  Pa_Sleep(44100);
+
+  return pa_err;
+}
+
+constexpr bool open_audio_as_defaulted(PaStream *audio_stream, char *user_data)
+{
+  AudioTestData *audio_data = (AudioTestData *)user_data;
+  PaError pa_err = Pa_OpenDefaultStream(&audio_stream, 0, 2, paFloat32, SAMPLE_RATE, 256, audio_test_callback, &audio_data);
+  if (pa_err == paNoError) {
+    pa_err = Pa_StopStream(audio_data);;
+  }
 
   return 0;
 }
