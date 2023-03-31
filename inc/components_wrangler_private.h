fda452de677bfd9db3d52a58e13d0278c9b35d8a
David <daniel.david.surla@gmail.com>
2023-03-31 20:38:00 +0100

    updated inc\components_wrangler_private.h...
    ...+-inc\components_wrangling.cpp

--- inc/components_wrangler_private.h
+++ inc/components_wrangler_private.h
@@ -1,51 +1,52 @@
 #ifndef COMPONENTS_H
 #define COMPONENTS_H
 
-#include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-enum class Errors
-{
+
+enum class Errors {
   None = 0,
   Found = 1,
 };
 
-template <typename T, typename E>
-class Result
-{
+template <typename T, typename E> class Result {
   Result(T type, E expected);
 };
 
 class UserName(string);
 class UserEntity;
-class UserBundle
-{
+class UserBundle {
   UserName *name;
   UserEntity *entity;
 };
 
-class UserCommand
-{
+class UserCommand {
 public:
   void execute_user_command();
 };
 
-class UserHandler : public UserCommand
-{
+class UserHandler : public UserCommand {
   UserCommand *user_command;
 
 public:
-  void execute_user_command()
-  {
+  void execute_user_command() {
     handle_user_command(*UserHandler::user_command);
   }
-  UserBundle* init_user_table()
-  {
+  UserBundle *init_user_table() {
     UserBundle *user_bundle = &UserBundle{};
     return user_bundle;
   }
   void handle_user_command(UserCommand user_command);
+};
+
+class LeftChannelPhase;
+class RightChannelPhase;
+
+class AudioTestData {
+  float left_channel_phase;
+  float right_channel_phase;
 };
 
 #endif // COMPONENTS_H
--- inc/components_wrangling.cpp
+++ inc/components_wrangling.cpp
@@ -1,6 +1,23 @@
+#define SAMPLE_RATE (44100)
 #include "components_wrangler_private.h"
 
-template <class T, class E>
-Result<T, E>::Result(T type, E expected) {}
+template <class T, class E> Result<T, E>::Result(T type, E expected) {}
 
 void UserHandler::handle_user_command(UserCommand user_command) {}
+
+static int audio_test_callback(const void *input_buffer, void *output_buffer,
+                               unsigned long frames_per_buffer,
+                               const PaStreamCallbackTimeInfo *time_info,
+                               PaStreamCallbackFlags status_flags,
+                               void *user_data) {
+  AudioTestData *audio_tester = (AudioTestData*)user_data;
+  float *output = (float*)output_buffer;
+  unsigned int index = 0;
+
+  // for (index = 0; index < frames_per_buffer; index++) {
+  //   output += audio_tester->left_channel_phase;
+  //   output += audio_tester->right_channel_phase;
+  // }
+
+  return 0;
+}
