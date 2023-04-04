807c3bf6f1a8453fd29e307f820f836be316e5fd
David <daniel.david.surla@gmail.com>
2023-04-04 08:36:59 +0100

    added .infra\scripts\vcpkg_inst_deps.sh
    This is so weird, why not just have this a part of the Makefile? Why a script? Can someone tell Danny Boy that having to rely on a Shellscript (Bash specifically) is a bad idea to build dependencies from like oh my life!

---
+++ .infra/scripts/vcpkg_inst_deps.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+function clone() { # vcpkg install dependencies
+    git clone "https://github.com/Microsoft/vcpkg.git"
+}
+
+function boot() {
+    ./vcpkg/bootstrap-vcpkg.sh
+}
+
+function install() {
+    vcpkg install portaudio
+    vcpkg install libsndfile
+}
+
+function make() {
+    input_toolchain_filepath=$1 # script argument 1
+    toolchain_file_expected = find / -name $input_toolchain_filepath -print | grep $input_toolchain_filepath
+    cmake -B /build -S . -DCMAKE_TOOLCHAIN_FILE=/$toolchain_file_expected/scripts/buildsystems/vcpkg.cmake
+}
+
+function build() {
+    cmake --build /build
+}
+
+function init() {
+    clone
+    boot
+    install
+    make $1
+    build
+}
