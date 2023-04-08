62c82a8488107eefce1bbff63b173c4c2ebf9bd9
David <daniel.david.surla@gmail.com>
2023-04-08 05:57:24 +0100

    updated README.md...
    ....
    slight modification in: recuse\src\lib.rs

--- recuse/src/lib.rs
+++ recuse/src/lib.rs
@@ -8,12 +8,11 @@ fn HandleAnimationBlock(ctx: Scope, data_size: i64) -> impl IntoView {
     let (viewmodel_signal, vdatum) = create_signal(ctx, data_size);
     let update = move |_| fdatum.update(|context_signal| *context_signal = 0);
     let placeholder = vdatum.update(|context_signal| *context_signal = 0);
-    
+
     view! {ctx,
         <div>
             <button on:click=update>{placeholder}</button>
         </div>
-
     }
 }
 
