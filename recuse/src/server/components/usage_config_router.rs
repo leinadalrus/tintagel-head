89f082cdb1eedf8a1fffb083f47d2a7aed30664d
David <daniel.david.surla@gmail.com>
2023-04-21 02:08:46 +0100

    Updated recuse\src\server\components\usage_config_router.rs ...
    ...
    modified:
    * recuse\src\server\components\mod.rs
    * recuse\Cargo.toml

--- recuse/src/server/components/usage_config_router.rs
+++ recuse/src/server/components/usage_config_router.rs
@@ -1,52 +1,70 @@
 use axum::{
+    extract::Query,
     http::StatusCode,
     response::{Html, IntoResponse},
-    routing::{get, post},
+    routing::{get, get_service, post},
     Json, Router,
 };
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
+use tower_http::services::ServeDir;
 
 // the input to our `index_page` handler
-#[derive(Deserialize)]
-struct IndexPage {
-    pagename: String,
+#[derive(Debug, Deserialize, Serialize)]
+struct QueryParameter {
+    query_arguments: Option<String>,
 }
 
 // the output to our `index_page` handler
-#[derive(Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 struct Page {
     id: u64,
     pagename: String,
 }
 
 async fn index_page(
-    Json(payload): Json<IndexPage>,
-) -> (StatusCode, Json<Page>) {
+    Query(parameters): Query<QueryParameter>,
+) -> impl IntoResponse {
     // insert your application logic here
     let page = Page {
         id: 0,
-        pagename: payload.pagename,
+        pagename: parameters.query_arguments.to_owned().unwrap(),
     };
 
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
-    (StatusCode::CREATED, Json(page))
+    println!(
+        "$=> {:<16} --- Query Sequence & Parameters Handler --- {parameters:?}",
+        "HANDLER"
+    );
+
+    let pathname = parameters
+        .query_arguments
+        .as_deref()
+        .unwrap_or("HTTP Error Code: 400");
+
+    return Html(format!("{pathname}"));
 }
 
-// basic handler that responds with a static string
-async fn root() -> &'static str { "/" }
+fn root() -> &str { return "/"; }
+
+fn routes_into_router() -> Router {
+    Router::new().route(root(), get(index_page))
+}
+
+fn nest_service_route() -> Router {
+    Router::new().nest_service(root(), get_service(ServeDir::new("./")))
+}
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let app_router = Router::new().route(
-        root(),
-        get(|| async { Html("<i>Hello, and good tidings.</i>") }),
-    );
+    let app_router = Router::new();
     let local_host_address = SocketAddr::from(([127, 0, 0, 1], 3000));
-    
+
+    let all_routes = app_router
+        .merge(routes_into_router())
+        .fallback_service(nest_service_route());
+
     tracing::debug!("Listening on: {:?}", local_host_address);
     axum::Server::bind(&local_host_address)
         .serve(app_router.into_make_service())
