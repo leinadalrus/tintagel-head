760923798b40c4915ca1e819174dc3e0ae1de8b0
David <daniel.david.surla@gmail.com>
2023-05-08 06:17:28 +0100

    Update amd64_comptime_cell_interpreter.hpp

--- lib/amd64_comptime_cell_interpreter.hpp
+++ lib/amd64_comptime_cell_interpreter.hpp
@@ -13,28 +13,13 @@ enum Amd64GuestPolicy {
   NOKS = 0x0001,
   ES = 0x0002,
   NOSEND = 0x0003,
-  DOMAIN = 0x0004,
+  GPDOMAIN = 0x0004,
   SEV = 0x0005,
   RESERVED = 0x0000,  // Reserved. Should be zero. Bit(s) are := 15:6
   API_MAJOR = 0x0007, // Bit(s) is := 7:0
   API_MINOR = 0x0007, // Bit(s) is := 7:0
 }; // The guest must not be transmitted to another platform with a lower
    // firmware version.
-
-typedef struct ArenaData {
-} ArenaData; // NOTE TODO Arrays-of-Structs
-
-typedef struct ArenaState {
-  enum Amd64GuestPolicy guest_policy;
-  ArenaData *arena_data;
-} ArenaState;
-
-typedef struct Amd64Interpreter {
-  enum Amd64GuestPolicy guest_policy;
-} Amd64Interpreter;
-
-typedef struct Amd64Recompiler {
-} Amd64Recompiler;
 
 // TODO decompiler/recompiler for cross-platform usage
 template <typename T> class Amd64ArenaAllocator {
