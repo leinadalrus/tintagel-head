b26b97248670db5c83408f06c1ef286cd64084b8
David <daniel.david.surla@gmail.com>
2023-05-09 22:35:12 +0100

    updated core/ and lib/

--- lib/amd64_comptime_cell_arenas.hpp
+++ lib/amd64_comptime_cell_arenas.hpp
@@ -1,10 +1,58 @@
-#ifndef AMD_X86_64_CELL_ARENAS_HPP
-#define AMD_X86_64_CELL_ARENAS_HPP
+#ifndef AMD_COMPTIME_CELL_ARENAS_HPP
+#define AMD_COMPTIME_CELL_ARENAS_HPP
 
 #include <cstdint>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <functional>
+
+namespace External {
+namespace Coreboot {
+enum class Amd64Entry {};
+};
+}; // namespace External
+
+namespace SynergisticProcessingUnit {
+namespace Lint {
+class Thread {};
+
+Thread *create_thread(std::function<void()> functor);
+void free_thread(Thread *thread);
+void block_thread(Thread *thread);
+
+class Mutex {};
+
+Mutex *create_mutex();
+void free_mutex(Mutex *mutex);
+void lock_mutex(Mutex *mutex);
+void unlock_mutex(Mutex *mutex);
+int try_lock_mutex(Mutex *mutex);
+
+void sleep_linting(uint64_t seconds);
+}; // namespace Lint
+}; // namespace SynergisticProcessingUnit
+
+namespace MicroProgramCounter {
+int initialize_microprogram_counter();
+void deinitialize_microprogram_counter();
+void begin_microprogram_counter();
+void end_microprogram_counter();
+int send_microprogram_counters_packet(uint8_t data, int length,
+                                      uint8_t timestamp);
+int receive_microprogram_counters_packet(uint8_t data, int length,
+                                         uint8_t timestamp);
+int send_microprogram_counters_command(uint8_t data, int length,
+                                       uint8_t timestamp);
+int send_microprogram_counters_reply(uint8_t data, uint8_t timestamp,
+                                     uint16_t bitmask);
+int send_microprogram_counters_acknowledgement(uint8_t data, int length,
+                                               uint8_t timestamp);
+int send_microprogram_counters_host_packet(uint8_t data, int length,
+                                           uint8_t timestamp);
+int receive_microprogram_counters_replies(uint8_t data, uint8_t timestamp,
+                                          uint16_t bitmask);
+}; // namespace MicroProgramCounter
 
 class ArenaData {
   void *destination, *source, *table_data;
@@ -29,4 +77,4 @@ protected:
                                   // inherited classes
 };
 
-#endif // AMD_X86_64_CELL_ARENAS_HPP
+#endif // AMD_COMPTIME_CELL_ARENAS_HPP
--- lib/amd64_comptime_spu_handler.hpp
+++ lib/amd64_comptime_spu_handler.hpp
@@ -1,15 +1,23 @@
 #ifndef AMD64_COMPTIME_SPU_HANDLER_HPP
 #define AMD64_COMPTIME_SPU_HANDLER_HPP
 
+#include "amd64_comptime_cell_arenas.hpp"
 #include <cstdint>
 #include <cstdio>
 
-class SynergyProcessHandler {
+class SynergisticProcessingUnitHandler {
   FILE *file;
 
 public:
-  void initialise_distributed_hashtable();
-  void deinitialise_distributed_hashtable();
+  int initialize_table_data();
+  void deinitialize_table_data();
+  void reset_processing_unit();
+  void stop_processing_unit();
+
+  void set_microprogram_power_count(uint32_t power_count);
+  void set_bias(uint16_t bias);
+  void set_degrade_bit_by_ten(int enable);
+  void set_apply_bias(int enable);
 
   uint8_t read_file_encoded_u8(uint32_t address_offset);
   uint16_t read_file_encoded_u16(uint32_t address_offset);
@@ -22,8 +30,35 @@ public:
                               uint32_t address_sizetype);
 };
 
-class SynergyProcessChannel {};
+template <typename T> class SynergisticProcessingUnitCapture {
+  uint32_t number;
+  uint32_t count;
+  uint32_t source_address;
+  uint32_t destination_address;
 
-class SynergyProcessUnitCapture {};
+  uint16_t timer_reload;
+  uint32_t cursor_position;
+  uint32_t length;
+
+  uint32_t fifo[8];
+  uint32_t fifo_read_position;
+  uint32_t fifo_write_position;
+  uint32_t fifo_read_offset;
+  uint32_t fifo_write_offset;
+  uint32_t fifo_level;
+
+  void fifo_flush_data();
+  T fifo_write_data();
+
+  void set_count(uint8_t data);
+  void set_destination_address(uint32_t data);
+  void set_timer_reload(uint32_t data);
+  void set_length(uint32_t);
+
+  void start();
+  void run(int32_t data);
+
+  void bus_write_u32(uint32_t address, uint64_t data);
+};
 
 #endif // AMD64_COMPTIME_SPU_HANDLER_HPP
